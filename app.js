125983ea6ee85ad4d9e0d152dd01bd7fb22615f5
anhtaiho <sadiomanehello@gmail.com>
2026-08-02 17:11:50 +0700

    video24

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-console.log("Hello, World!");
-console.log("hoidanit");
+const  express = require('express');
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/hoidanit", (req, res) => {
+  res.send("Hello HoiDanit!");
+});
+
+app.listen(8080, () => {
+    console.log(`My app is running on port: ${PORT}`);
+})   

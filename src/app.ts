48679a527683f42894c4a390878888232fca995b
anhtaiho <sadiomanehello@gmail.com>
2026-08-03 15:01:52 +0700

    video 31

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,17 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!, nodemon");
+  res.send(`<h1 style="color: red;">Hello HoiDanit!</h1>`);
 });
 
 app.get("/hoidanit", (req, res) => {
   res.send("Hello HoiDanit!");
 });
 
+app.get("/abc", (req, res) => {
+  res.send("Hello abc!");
+});
+
 app.listen(PORT, () => {
   console.log(`My app is running on port: ${PORT}`);
-  console.log("env PORT:", process.env.PORT);
 })   

4b5b3b4a52cf9baa365bab4fd8a874883d0e000d
anhtaiho <sadiomanehello@gmail.com>
2026-08-06 15:56:50 +0700

    video 39

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,16 @@ const PORT = process.env.PORT || 8080;
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 
-//config routes
-import webRoutes from './routes/web';
-webRoutes(app);
+//config req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // config static files
 app.use(express.static('public'));
+
+//config routes
+import webRoutes from './routes/web';
+webRoutes(app);
 
 app.listen(PORT, () => {
   console.log(`My app is running on port: ${PORT}`);
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { handleCreateUser } from "../services/user.service";
 
 const getHomePage = (req: Request, res: Response) => {
     return res.render("home");
@@ -9,7 +10,13 @@ const getCreateUserPage = (req: Request, res: Response) => {
 }
 
 const postCreateUser = (req: Request, res: Response) => {
-    console.log(">>> check data: ", req.body);
+    
+    const { fullname, email, address } = req.body;
+
+    // handle create user
+    handleCreateUser(fullname, email, address);
+
+    // handle create user
     return res.redirect("/");
 }
 
